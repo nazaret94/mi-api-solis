b0846d5e013a651db21afb5d10b7f4d2e803c328
Mariana <z.areth94@hotmail.com>
2025-10-24 19:57:53 -0600

    Se corrigio alumno.controller y alumno.model

--- src/Controllers/alumno.controller.js
+++ src/Controllers/alumno.controller.js
@@ -2,12 +2,11 @@ import{ getAlumno } from "../models/alumno.model.js"
 
 //Get api/alumno
 export const listarAlumno = async(req, res)=>{
-    try{
-        const data = await getAlumno();
-        res.status(200).json(data)
-    }
-    catch(err){
-        console.error("Ocurrio un error", err)
-        res.status(500).json({err: "Error al listar al alumno"})
-    }
+    try {
+    const data = await getAlumno();
+    res.status(200).json([data]); 
+    } catch (err) {
+    console.error("Ocurrió un error:", err);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 };
--- src/models/alumno.model.js
+++ src/models/alumno.model.js
@@ -1,9 +1,45 @@
 import { pool } from "../config/db.js";
 
 export async function getAlumno(){
-    const[resultSets] = await pool.query("CALL obtener_alumno_completo();");
-    const first = Array.isArray(resultSets) ? resultSets[0] ?? resultSets : resultSets;
-    return first;
+     try {
+    const [resultSets] = await pool.query(
+      `CALL obtener_alumno_completo();` 
+    );
+
+    let perfil = resultSets[0]?.[0]?.perfil_completo;
+    if (!perfil) perfil = "{}";
+
+    perfil = JSON.parse(perfil);
+
+    return {
+      alumno: {
+        id_alumno: perfil.alumno?.id_alumno ?? 0,
+        nombre: perfil.alumno?.nombre ?? "string",
+        correo: perfil.alumno?.correo ?? "user@example.com",
+        telefono: perfil.alumno?.telefono ?? "string",
+        github: perfil.alumno?.github ?? "Unknown Type: string,null",
+        linkedin: perfil.alumno?.linkedin ?? "Unknown Type: string,null"
+      },
+      educacion: (perfil.educacion?.length
+        ? perfil.educacion
+        : [{ id_educacion: 0, nombre: "string", institucion: "string", fecha_inicio: "Unknown Type: string,null", fecha_fin: "Unknown Type: string,null", logo: "Unknown Type: string,null" }]
+      ),
+      experiencia: (perfil.experiencia?.length
+        ? perfil.experiencia
+        : [{ id_experiencia: 0, empresa: "string", puesto: "string", fecha_inicio: "Unknown Type: string,null", fecha_fin: "Unknown Type: string,null", descripcion: "string", logo: "Unknown Type: string,null" }]
+      ),
+      habilidades: (perfil.habilidades?.length
+        ? perfil.habilidades
+        : [{ id_habilidades: 0, nombre: "string", nivel: "Unknown Type: string,null", logo: "Unknown Type: string,null" }]
+      ),
+      proyectos: perfil.proyectos?.length ? perfil.proyectos : "Unknown Type: array,null",
+      mensajes: perfil.mensajes?.length ? perfil.mensajes : "Unknown Type: array,null"
+    };
+
+  } catch (err) {
+    console.error("Error en getAlumno:", err);
+    throw err;
+  }
 }
 
 
